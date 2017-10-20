b316cab42f769d795b796c31b919daf839d84ba2
AzureQ <qi.shao.nyu@gmail.com>
2017-10-20 19:16:42 +0000

    my_answers version1

--- my_answers.py
+++ my_answers.py
@@ -13,6 +13,11 @@ def window_transform_series(series, window_size):
     X = []
     y = []
 
+    for idx,each in enumerate(series):
+        if (idx + window_size) < len(series):
+            X.append(series[idx:idx+window_size])
+            y.append(series[idx+window_size])
+            
     # reshape each 
     X = np.asarray(X)
     X.shape = (np.shape(X)[0:2])
@@ -23,13 +28,19 @@ def window_transform_series(series, window_size):
 
 # TODO: build an RNN to perform regression on our time series input/output data
 def build_part1_RNN(window_size):
-    pass
+    model = Sequential()
+    model.add(LSTM(5, input_shape = (window_size,1)))
+    model.add(Dense(1))
+    return model
 
 
 ### TODO: return the text input with only ascii lowercase and the punctuation given below included.
 def cleaned_text(text):
+    from string import ascii_lowercase
     punctuation = ['!', ',', '.', ':', ';', '?']
-
+    for c in set(text):
+        if (c not in punctuation) and (c not in ascii_lowercase):
+            text = text.replace(c,' ')
     return text
 
 ### TODO: fill out the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model
@@ -37,10 +48,18 @@ def window_transform_text(text, window_size, step_size):
     # containers for input/output pairs
     inputs = []
     outputs = []
+    
+    for idx in range(0, len(text),step_size):
+        if (idx + window_size) < len(text):
+            inputs.append(text[idx : idx+window_size])
+            outputs.append(text[idx+window_size])
 
     return inputs,outputs
 
 # TODO build the required RNN model: 
 # a single LSTM hidden layer with softmax activation, categorical_crossentropy loss 
 def build_part2_RNN(window_size, num_chars):
-    pass
+    model = Sequential()
+    model.add(LSTM(200, input_shape=(window_size,num_chars)))
+    model.add(Dense(num_chars, activation='softmax'))
+    return model
